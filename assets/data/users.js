6c61a2cb9848044eeb6f81102fb108fc70d4099f
Mobin Akhter <55329336+MobinAkhter@users.noreply.github.com>
2023-05-04 15:42:25 -0400

    Add files via upload

---
+++ assets/data/users.js
@@ -0,0 +1,90 @@
+export default [
+  {
+    id: "1",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+    name: "Elon",
+    handle: "elon",
+    bio: 'OnlyFans is an internet content subscription service based in London, United Kingdom. It hosts the work of content creators, such as physical fitness experts and musicians. Content creators can earn money from users who subscribe to their content—the "fans". It allows content creators to receive funding directly from their fans on a monthly basis as well as one-time tips and the pay-per-view feature. ',
+    subscriptionPrice: 0,
+  },
+  {
+    id: "2",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+    name: "Jeff",
+    handle: "jeff",
+    bio: "hello",
+    subscriptionPrice: 4.99,
+  },
+  {
+    id: "3",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg",
+    name: "Biahaze",
+    handle: "biahaze",
+    bio: "hello",
+    subscriptionPrice: 2.99,
+  },
+  {
+    id: "4",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/3.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+    name: "Elon",
+    handle: "elon2",
+    bio: "hello",
+    subscriptionPrice: 1.99,
+  },
+  {
+    id: "5",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/4.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg",
+    name: "Graham",
+    handle: "graham",
+    bio: "hello",
+    subscriptionPrice: 0,
+  },
+  {
+    id: "6",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+    name: "Jeff",
+    handle: "jeff2",
+    bio: "hello",
+    subscriptionPrice: 3.99,
+  },
+  {
+    id: "7",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/6.jpg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
+    name: "Vadim",
+    handle: "vadim",
+    bio: "hello",
+    subscriptionPrice: 1.99,
+  },
+  {
+    id: "8",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/7.jpeg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG",
+    name: "Vadim",
+    handle: "vadim2",
+    bio: "hello",
+    subscriptionPrice: 0,
+  },
+];
